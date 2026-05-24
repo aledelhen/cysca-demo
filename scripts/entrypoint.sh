52e665223effa2215538cb6f879c75084917331e
alaa <alaaeddine.elhennach@gmail.com>
2026-05-24 13:11:36 -0400

    config fix 13

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,4 +10,4 @@ export PYTHONPATH=/workspace
 export GAUGE_PYTHON_COMMAND="${GAUGE_PYTHON_COMMAND:-/app/.venv/bin/python}"
 
 cd /workspace
-exec gauge run specs --env docker
+exec gauge run specs --env docker --log-level debug
