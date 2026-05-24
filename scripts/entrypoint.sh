01d047562bf22f7871ae00ca4367fc414d545e77
alaa <alaaeddine.elhennach@gmail.com>
2026-05-24 13:25:48 -0400

    config fix 16

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -eu
 
-mkdir -p /home/go/.gauge/config /home/go/.gauge/plugins /home/go/.gauge/logs
-mkdir -p /workspace/reports /workspace/logs
-chown -R 997:984 /home/go /workspace
-
 export HOME=/home/go
 export PYTHONPATH=/workspace
 export GAUGE_PYTHON_COMMAND="${GAUGE_PYTHON_COMMAND:-/app/.venv/bin/python}"
