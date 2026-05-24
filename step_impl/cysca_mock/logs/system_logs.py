000a317ddc5629221680035d58aefc9f2f92dda7
alaa <alaaeddine.elhennach@gmail.com>
2026-05-24 13:19:42 -0400

    config fix 15

---
+++ step_impl/cysca_mock/logs/system_logs.py
@@ -0,0 +1,6 @@
+def startup_log_lines():
+    """Return the simulated controller startup log lines."""
+    return [
+        "INFO controller boot sequence started",
+        "INFO controller health check passed",
+    ]
